2f4f445b6053cf9aff7b75264afa53ba9d54d092
muhammadislom <islom.pr@gmail.com>
2022-10-03 17:53:15 +0500

    update

--- src/Providers/OneIdServiceProvider.php
+++ src/Providers/OneIdServiceProvider.php
@@ -8,10 +8,9 @@ class oneIdServiceProvider extends ServiceProvider
 
     public function boot()
     {
-//        $path = realpath(__DIR__.'/../../config/config.php');
-//        $this->publishes([$path => config_path('oneId.php')], 'config');
-//        $this->mergeConfigFrom($path, 'oneId');
-        $this->mergeConfigFrom(__DIR__.'../../config/config.php', 'oneId');
+        $path = realpath(__DIR__.'/../../config/config.php');
+        $this->publishes([$path => config_path('oneId.php')], 'config');
+        $this->mergeConfigFrom($path, 'oneId');
     }
 
 //    public function register()
