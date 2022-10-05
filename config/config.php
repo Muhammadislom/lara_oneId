e34c1931b00cdce083aa6470242f16b5c1c388d6
muhammadislom <islom.pr@gmail.com>
2022-10-05 09:47:42 +0500

    update

--- config/config.php
+++ config/config.php
@@ -2,10 +2,16 @@
 
 return [
     'authorizationUrl' => env( 'ONEID_AUTHORIZATION_URL', 'https://sso.egov.uz/sso/oauth/Authorization.do' ),
+
     'responseType' => env( 'ONEID_RESPONSE_TYPE', null),
+
     'clientId' => env( 'ONEID_CLIENT_ID', null ),
-    'redirectUri' => env( 'ONEID_REDIRECT_URI', null ),
+
+    'redirectUri' => route('login'),
+
     'clientSecret' => env( 'ONEID_CLIENT_SECRET', null),
+
     'scope' => env( 'ONEID_SCOPE', null ),
+
     'stateArr' => [ 'method' => null ],
 ];
